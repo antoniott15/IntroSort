081d7a844fc1722144c96c8e2133870e625c1c4b
Antonio <antonio.toche@utec.edu.pe>
2019-04-05 22:12:14 -0500

    v2.0

--- main.cpp
+++ main.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 
 int partition(int[], int, int);
-void InsertionSort(int[], int, int);
+void InsertionSort(int, int, int);
 int randPartition(int[], int, int);
 void HeapSort(int *, int *);
 void introsort(int[], int *, int *, int);
 
 int main()
 {
-    int a[] = {5, 7, -8, 9, 10, 4, -7, 0, -12, 1, 6, 2, 3, -4, -15, 12, 100};
+    int a[] = {5, 7, -8, 9, 10, 4, -7, 0, -12, 1, 6, 2, 3, -4, -15, 12};
     int n = sizeof(a) / sizeof(a[0]);
 
-    int maxdepth = log(n) * 2;
+    int maxCValues = log(n) * 2;
 
-    introsort(a, a, a + n - 1, maxdepth);
+    introsort(a, a, a + n - 1, maxCValues);
 
     for (int i = 0; i < n; i++)
         cout << a[i] << " ";
@@ -34,26 +34,29 @@ void InsertionSort(int a[], int low, int high)
 
 int randPartition(int a[], int low, int high)
 {
+    int pivotIndex = rand() % (high - low + 1) + low;
+    swap(a[pivotIndex], a[high]);
+    return partition(a, low, high);
 }
 
 void HeapSort(int *begin, int *end)
 {
 }
 
-void introsort(int a[], int *begin, int *end, int maxdepth)
+void introsort(int a[], int *begin, int *end, int maxCValues)
 {
     if ((end - begin) < 16)
     {
         InsertionSort(a, begin - a, end - a);
     }
-    else if (maxdepth == 0)
+    else if (maxCValues == 0)
     {
         HeapSort(begin, end + 1);
     }
     else
     {
         int pivot = randPartition(a, begin - a, end - a);
-        introsort(a, begin, a + pivot - 1, maxdepth - 1);
-        introsort(a, a + pivot + 1, end, maxdepth - 1);
+        introsort(a, begin, a + pivot - 1, maxCValues - 1);
+        introsort(a, a + pivot + 1, end, maxCValues - 1);
     }
 }
